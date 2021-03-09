1b94a5d513b0e78e3d42987409d7ae8c94665ad4
srpg <44681214+srpg@users.noreply.github.com>
2021-03-09 20:31:25 +0200

    Removed in admin ability update, seems cause crash

--- addons/source-python/plugins/zr/modules/admin/__init__.py
+++ addons/source-python/plugins/zr/modules/admin/__init__.py
@@ -30,31 +30,10 @@ def adminmenu(userid):
 		return
 	menu.append(Text('Admin Menu\nSection: Main'))
 	menu.append(Text(' '))
-	menu.append(SimpleOption(1, 'Updates', 'update'))
-	menu.append(SimpleOption(2, 'Days',	'day'))
+	menu.append(SimpleOption(1, 'Days',	'day'))
 	menu.append(Text(' '))
 	menu.append(SimpleOption(0, 'Close', None))
 	menu.select_callback = admin_menu_callback
-	menu.send(index_from_userid(userid))
-
-def update_menu(userid):
-	menu = SimpleMenu()
-	if zr.is_queued(menu, index_from_userid(userid)):
-		return
-	menu.append(Text('Admin Menu\nSection: Update'))
-	menu.append(Text(' '))
-	if version.version_checker() > version.Ver:
-		menu.append(Text('There is new version available to download!'))
-		menu.append(Text(' '))
-		menu.append(SimpleOption(1, 'Update', 'Install'))
-		menu.append(SimpleOption(2, 'Reload Zombie Riot', 'Reload'))
-	else:
-		menu.append(Text('There is no new available to download!'))
-		menu.append(Text(' '))
-		menu.append(SimpleOption(1, 'Reload Zombie Riot', 'Reload'))
-	menu.append(Text(' '))
-	menu.append(SimpleOption(0, 'Close', None))
-	menu.select_callback = update_menu_callback
 	menu.send(index_from_userid(userid))
 
 def day_menu(userid):
@@ -72,19 +51,7 @@ def days_menu_callback(_menu, _index, _option):
 		userid = userid_from_index(_index)
 		zr._day = choice
 		SayText2('\x04[Zombie Riot] » You have changed day to %s!' % (choice)).send(index_from_userid(userid))
-		
 
-def update_menu_callback(_menu, _index, _option):
-	choice = _option.value
-	if choice:
-		userid = userid_from_index(_index)
-		if choice == 'Install':
-			queue_command_string('zombie_download')
-			SayText2('\x04[Zombie Riot] » You have downloaded new version of zombie riot. \n\x04[Zombie Riot] » For apply the changes reload the plugin').send(index_from_userid(userid))
-		elif choice == 'Reload':
-			queue_command_string('sp plugin reload zr')
-			SayText2('\x04[Zombie Riot] » You have reloaded zombie riot plugin!').send(index_from_userid(userid))
-			
 def admin_menu_callback(_menu, _index, _option):
 	choice = _option.value
 	if choice:
