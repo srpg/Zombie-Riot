68210ce9bed904fccebe917fc07522b3a573c46a
srpg <44681214+srpg@users.noreply.github.com>
2021-03-09 14:21:15 +0200

    Added console command to download new version
    Huge thanks to L'In20Cible for helping

--- addons/source-python/plugins/zr/modules/version/__init__.py
+++ addons/source-python/plugins/zr/modules/version/__init__.py
@@ -1,18 +1,43 @@
+from io import BytesIO
+from path import Path
+from paths import GAME_PATH
 from urllib.request import urlopen
+from zipfile import ZipFile
 from core import echo_console
+from commands.server import ServerCommand
 
-Ver = '1.0.3'
-
+Ver = '1.0.4'
+download_url = ('https://github.com/srpg/Zombie-Riot/archive/main.zip')
 new_version = ('http://cssbestrpg.online/version.txt')
+UPDATE_PATH = GAME_PATH
 
 def version_checker(timeout=3):
-    with urlopen(new_version, timeout=timeout) as url:
-        return url.read().decode('utf-8')
+	with urlopen(new_version, timeout=timeout) as url:
+		return url.read().decode('utf-8')
 
 def check_version():
 	if version_checker() > Ver:
-		echo_console('[Zombie Riot] There is %s version available to download!' % (version_checker()))
-		echo_console('[Zombie Riot] You can download new version from: https://github.com/srpg/Zombie-Riot')
-		echo_console('[Zombie Riot] You are currently running %s version' % (Ver))
+		echo_console('[Zombie Riot] There is new version available to download!')
+		echo_console('[Zombie Riot] Type in console zombie_download to download new version')
 	else:
 		echo_console('[Zombie Riot] There is no new version available!')
+
+def download(timeout=3):
+	with urlopen('https://github.com/srpg/Zombie-Riot/archive/main.zip') as response:
+		with ZipFile(BytesIO(response.read())) as zipfile:
+			for info in zipfile.infolist():
+				if info.is_dir():
+					continue
+				filename = info.filename
+				path = GAME_PATH / Path(filename.split('/', 1)[1])
+				path.dirname().makedirs_p()
+				with open(path, 'wb') as f:
+					f.write(zipfile.read(filename))
+
+@ServerCommand('zombie_download')
+def zombie_downloadd(command):
+	if version_checker() > Ver:
+		download()
+		echo_console('[Zombie Riot] You have download newest version of the plugin. Restart a server to apply changes')
+	else:
+		echo_console('[Zombie Riot] There is not new build available to download')
