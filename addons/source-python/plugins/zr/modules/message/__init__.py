0bcd8149461a31e3caf22e5170956b2f8b792d55
srpg <44681214+srpg@users.noreply.github.com>
2021-03-31 21:27:50 +0300

    Started making translate messages for menus

--- addons/source-python/plugins/zr/modules/message/__init__.py
+++ addons/source-python/plugins/zr/modules/message/__init__.py
@@ -9,7 +9,10 @@ chat = LangStrings(__FILEPATH__ + '/messages/chat')
 # Chat Messages
 #===================
 
-Game = SayText2(chat['Game'])
-Market = SayText2(chat['Market'])
-Won = SayText2(chat['Win'])
-New = SayText2(chat['Restart'])
+Game    = SayText2(chat['Game'])
+Market  = SayText2(chat['Market'])
+Won     = SayText2(chat['Win'])
+New     = SayText2(chat['Restart'])
+Weapon  = SayText2(chat['Buy'])
+Potion  = SayText2(chat['Potion'])
+Rebuy   = SayText2(chat['Re_buy'])
