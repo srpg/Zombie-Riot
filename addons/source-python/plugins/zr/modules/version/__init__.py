f173b1aebcd0159af8a224583acbb46212402407
srpg <44681214+srpg@users.noreply.github.com>
2021-04-01 19:18:20 +0300

    Fixed not loading zombie plugin

--- addons/source-python/plugins/zr/modules/version/__init__.py
+++ addons/source-python/plugins/zr/modules/version/__init__.py
@@ -6,7 +6,7 @@ from zipfile import ZipFile
 from core import echo_console
 from commands.server import ServerCommand
 
-Ver = '1.1.4'
+Ver = '1.1.5'
 new_version = ('http://cssbestrpg.online/version.txt')
 UPDATE_PATH = GAME_PATH
 
