15cb78a70034a94082634556ab4ee26224906f44
srpg <44681214+srpg@users.noreply.github.com>
2021-05-04 18:28:33 +0300

    Updated version
    Adds some bug fixes

--- addons/source-python/plugins/zr/modules/version/__init__.py
+++ addons/source-python/plugins/zr/modules/version/__init__.py
@@ -6,7 +6,7 @@ from zipfile import ZipFile
 from core import echo_console
 from commands.server import ServerCommand
 
-Ver = '1.2.0'
+Ver = '1.2.1'
 new_version = ('http://cssbestrpg.online/version.txt')
 UPDATE_PATH = GAME_PATH
 
