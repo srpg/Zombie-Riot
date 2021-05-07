1687d2cba8a8041d1e51b87a3f9a97364f93e594
srpg <44681214+srpg@users.noreply.github.com>
2021-05-07 18:18:34 +0300

    Add files via upload

--- addons/source-python/plugins/zr/modules/version/__init__.py
+++ addons/source-python/plugins/zr/modules/version/__init__.py
@@ -7,7 +7,7 @@ from zipfile import ZipFile
 from core import echo_console
 from commands.server import ServerCommand
 
-Ver = '1.2.6'
+Ver = '1.2.7'
 new_version = ('https://cssbestrpg.online/version.txt')
 UPDATE_PATH = GAME_PATH
 
