f8904d6b496c9e3c103f8a33472982549b3bb5e7
srpg <44681214+srpg@users.noreply.github.com>
2021-06-16 15:33:47 +0300

    Update __init__.py

--- addons/source-python/plugins/zr/modules/version/__init__.py
+++ addons/source-python/plugins/zr/modules/version/__init__.py
@@ -7,7 +7,7 @@ from zipfile import ZipFile
 from core import echo_console
 from commands.server import ServerCommand
 
-Ver = '1.4.8'
+Ver = '1.4.9'
 new_version = ('https://cssbestrpg.online/version.txt')
 UPDATE_PATH = GAME_PATH
 
