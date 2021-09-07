66c211959871607236ebf5f5f4634a917bdafdec
srpg <44681214+srpg@users.noreply.github.com>
2021-09-07 21:36:39 +0300

    Update zr.py

--- addons/source-python/plugins/zr/zr.py
+++ addons/source-python/plugins/zr/zr.py
@@ -242,8 +242,8 @@ def get_speed(value):
 
 def get_boss(value):
 	val = '%s' % (value)
-	if val in int(_CONFIG['boss'])
-		return int(_CONFIG['boss'])
+	if not val >= max_day():
+		return int(_CONFIG['boss']['value'])
 
 def set_download():
 	echo_console('[Zombie Riot] Setting downloads!')
